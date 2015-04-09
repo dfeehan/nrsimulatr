b8d822dd9fe1476fb5df4bd4c2923fc6d81e289a
dennis feehan <dfeehan@princeton.edu>
2015-04-09 18:33:23 -0400

    fixed small labeling bug in case where block has 0 vertices

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -6,6 +6,7 @@
 #' @param block.names a vector (same length as \code{block.sizes}) with names of each group
 #' @export
 label.vertices <- function(graph, block.sizes, block.names) {
+
     V(graph)$group <- "none"
 
     break.start <- c(0, cumsum(block.sizes))
@@ -14,7 +15,10 @@ label.vertices <- function(graph, block.sizes, block.names) {
     break.start <- break.start[-length(break.start)] + 1
 
     for (i in 1:length(block.sizes)) {
-        V(graph)[break.start[i]:break.ends[i]]$group <- block.names[i]
+        # when this condition is FALSE, block i has 0 members (so we don't label anything)
+        if(break.start[i] <= break.ends[i]) {
+            V(graph)[break.start[i]:break.ends[i]]$group <- block.names[i]
+        }
     }
 
     return(graph)
