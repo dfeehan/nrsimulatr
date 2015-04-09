d3d1694d28dbe5c7bdfbeaee7238c26a33b50572
dennis feehan <dfeehan@princeton.edu>
2015-04-09 12:41:18 -0400

    added package title and description

--- R/nrsimulatr_package.R
+++ R/nrsimulatr_package.R
@@ -1,7 +1,14 @@
+#' network reporting simulations
+#'
+#' Utilities for drawing random social networks, reporting networks,
+#' and network reporting data using different assumptions about the process
+#' that generates connections between the nodes. Useful for conducting simulation
+#' studies to learn about sampling properties of network reporting and other
+#' network sampling methods.
+#'
 #' @docType package
 #' @name nrsimulatr
 #'
-#' @useDynLib mortfit
 #' @importFrom Rcpp sourceCpp
 #' @import igraph
 NULL
